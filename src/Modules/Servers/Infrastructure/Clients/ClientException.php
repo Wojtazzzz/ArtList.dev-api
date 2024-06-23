1d7473115faa55c97f5eab45b328a86e7711e15c
Wojtazz <marcin.witas72@gmail.com>
2024-06-23 10:16:36 +0200

    refactor(servers)/add missing final keyword to ClientException

--- src/Modules/Servers/Infrastructure/Clients/ClientException.php
+++ src/Modules/Servers/Infrastructure/Clients/ClientException.php
@@ -4,7 +4,7 @@ declare(strict_types=1);
 
 namespace App\Modules\Servers\Infrastructure\Clients;
 
-class ClientException extends \Exception
+final class ClientException extends \Exception
 {
     public function __construct()
     {
