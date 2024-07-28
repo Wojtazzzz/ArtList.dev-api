4db4a69a58d39e3b8a17c8e79a1e74866b8a3094
Wojtazz <marcin.witas72@gmail.com>
2024-07-28 19:42:36 +0200

    fix(AddServerCommand error)/add nullable motd

--- src/Modules/Servers/Domain/ValueObjects/Motd.php
+++ src/Modules/Servers/Domain/ValueObjects/Motd.php
@@ -1,15 +1,17 @@
 <?php
 
+declare(strict_types=1);
+
 namespace App\Modules\Servers\Domain\ValueObjects;
 
 final readonly class Motd
 {
-    public string $firstLine;
-    public string $secondLine;
+    public ?string $firstLine;
+    public ?string $secondLine;
 
-    public function __construct(string $firstLine, string $secondLine)
+    public function __construct(?string $firstLine, ?string $secondLine)
     {
-        $this->firstLine = trim($firstLine);
-        $this->secondLine = trim($secondLine);
+        $this->firstLine = $firstLine ? trim($firstLine) : null;
+        $this->secondLine = $secondLine ? trim($secondLine) : null;
     }
 }
